b1f1f37d17574494b5482ce345dc0be7254728dc
iqbalmiftahud13 <iqbalmiftahud@gmail.com>
2024-08-22 21:45:23 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+
+import { NuxtPage } from '#build/components';
+<template>
+  <div id="app">
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+#app {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
